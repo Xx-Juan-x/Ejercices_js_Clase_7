eee2a39810d6eb781ba00ac52a9d6fb25a219d64
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-05-25 16:12:52 -0300

    Variables y Operadores

---
+++ excercises/01_variables_and_operators.js
@@ -0,0 +1,18 @@
+//VARIABLES Y OPERADORES
+/*Crear dos variables de tipo String y concatenarlas guardando
+el resultado en una 3er variable. (utilizar concat)*/
+
+//var nombre = "Juan Ignacio";
+//var apellido = "Amarillo Jacob";
+//var nombreCompleto = nombre.concat(" ", apellido);
+//document.write("Nombre completo: ", nombreCompleto);
+
+/*Crear dos variables de tipo String y sumar el largo de cada
+variable (cantidad de letras del string) guardando el resultado
+de la suma en una 3er variable (utilizar length).*/
+
+
+//var cantLetras1 = "Fuí al kiosco y me compré un caramelo";
+//var catnLetras2 = "¿Cuánto te salió?";
+//var cantidadLetrasString = cantLetras1.concat(" ", catnLetras2);
+//document.write(cantidadLetrasString, " | Cantidad de letras del string: ", cantidadLetrasString.length);
