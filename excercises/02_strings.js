00e4aee1fa3d0e440918335c8bfe34fa2248cb65
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-05-25 22:57:20 -0300

    string

--- excercises/02_strings.js
+++ excercises/02_strings.js
@@ -1,7 +1,5 @@
-/*Crear una variable de tipo string con al menos 10
-caracteres y generar un nuevo string con los primeros
-5 caracteres guardando el resultado en una nueva variable
-(utilizar substring).*/
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros
+5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
 
 //var caracteres10 = "hamburguesa";
 //var caracteres5 = caracteres10.substring(0,5);
@@ -9,10 +7,8 @@ caracteres y generar un nuevo string con los primeros
 
 
 
-/*Crear una variable de tipo string con al menos 10 caracteres y generar
-un nuevo string con la primera letra en mayúscula y las demás en minúscula.
-Guardar el resultado en una nueva variable (utilizar substring, toUpperCase,
-toLowerCase y el operador +).*/
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula.
+Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
 
 //slice obtiene el residuo del string y lo convierto en minúscula
 //charAt 0 indico el primer caracter y que lo convierta en mayúscula
@@ -23,9 +19,7 @@ toLowerCase y el operador +).*/
 
 
 
-/*Crear una variable de tipo string con al menos 10 caracteres y
-algún espacio en blanco. Encontrar la posición del primer espacio
-en blanco y guardarla en una variable (utilizar indexOf).*/
+/*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
 
 //var localidad = "Lucas Gonzalez Entre Ríos";
 //var espacioCaracter = localidad.indexOf(" ");
